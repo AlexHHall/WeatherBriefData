5d381700bc99f7b0ba34ca42851b44962f208505
Alexander Hall <137228056+AlexHHall@users.noreply.github.com>
2024-08-14 14:38:04 +1200

    Server Test Data Added

---
+++ server.js
@@ -0,0 +1,22 @@
+const Express = require('express');
+const data = require('./data.json');
+const app = Express();
+var cors = require('cors')
+app.use(cors())
+
+const PORT = 3001;
+
+app.get('/api/airport/met/:icao', (req, res) => {
+  let airport_icao = req.params.icao;
+    let airport_data = data[airport_icao];
+    if (airport_data) {
+        res.json(airport_data);
+    } else {
+        res.json({ error: 'Airport not found' });
+    }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
